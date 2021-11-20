690767863a7b51fb233dd6dc2f0693eba9eb5f43
Linda Fang <fang4@clemson.edu>
2021-11-20 16:54:17 -0500

    changed naming convention

--- src/song.py
+++ src/song.py
@@ -1,34 +1,34 @@
 class Song:
 
-    def __init__(self, name=None, duration=None, fullAudio=None, genre=None, vocalAudio=None, timeSignature=None,
-                 tempo=None, mode=None, key=None, instrumentalness=None, instrumentalAudio=None):
+    def __init__(self, name=None, duration=None, full_audio=None, genre=None, vocal_audio=None, time_signature=None,
+                 tempo=None, mode=None, key=None, instrumentalness=None, instrumental_audio=None):
         # overloaded constructor
         if name:
             self.name = name
             self.duration = duration
-            self.fullAudio = fullAudio
+            self.full_audioudio = full_audio
             self.genre = genre
-            self.vocalAudio = vocalAudio
-            self.timeSignature = timeSignature
+            self.vocal_audio = vocal_audio
+            self.time_signature = time_signature
             self.tempo = tempo
             self.mode = mode
             self.key = key
             self.instrumentalness = instrumentalness
-            self.instrumentalAudio = instrumentalAudio
+            self.instrumental_audio = instrumental_audio
 
         # default constructor
         else:
             self.name = ""
             self.duration = 0
-            self.fullAudio = "/"
+            self.full_audio = "/"
             self.genre = ""
-            self.vocalAudio = "/"
-            self.timeSignature = 0
+            self.vocal_audio = "/"
+            self.time_signature = 0
             self.tempo = 0
             self.mode = True
             self.key = 0
             self.instrumentalness = 0.0
-            self.instrumentalAudio = "/"
+            self.instrumental_audio = "/"
 
     # getter/setter methods
     def set_name(self, n):
@@ -43,11 +43,11 @@ class Song:
     def get_duration(self):
         return self.duration
 
-    def set_fullAudio(self, fA):
-        self.fullAudio = fA
+    def set_full_audio(self, fa):
+        self.full_audio = fa
 
-    def get_fullAudio(self):
-        return self.fullAudio
+    def get_full_audio(self):
+        return self.full_audio
 
     def set_genre(self, g):
         self.genre = g
@@ -55,17 +55,17 @@ class Song:
     def get_genre(self):
         return self.genre
 
-    def set_vocalAudio(self, vA):
-        self.vocalAudio = vA
+    def set_vocal_audio(self, va):
+        self.vocal_audio = va
 
-    def get_vocalAudio(self):
-        return self.vocalAudio
+    def get_vocal_audio(self):
+        return self.vocal_audio
 
-    def set_timeSignature(self, tS):
-        self.timeSignature = tS
+    def set_time_signature(self, ts):
+        self.time_signature = ts
 
-    def get_timeSignature(self):
-        return self.timeSignature
+    def get_time_signature(self):
+        return self.time_signature
 
     def set_tempo(self, t):
         self.tempo = t
@@ -91,8 +91,8 @@ class Song:
     def get_instrumentalness(self):
         return self.instrumentalness
 
-    def set_instrumentalAudio(self, iA):
-        self.instrumentalAudio = iA
+    def set_instrumental_audio(self, ia):
+        self.instrumental_audio = ia
 
-    def get_instrumentalAudio(self):
-        return self.instrumentalAudio
+    def get_instrumental_audio(self):
+        return self.instrumental_audio
